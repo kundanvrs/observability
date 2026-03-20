84c89325ccf348827afd0e4acccf6212c787bfe0
Kundan Kumar <kundanvrs@gmail.com>
2026-03-21 01:05:13 +0530

    Refactor alert.py to use dynamic email credentials

--- scripts/availability-app1/alert.py
+++ scripts/availability-app1/alert.py
@@ -1,14 +1,14 @@
 import smtplib
-from config import ALERT_EMAIL
+from config import from_addr,to_addr,APP_PASS
 
 
 def send_alert(service):
     try:
         server = smtplib.SMTP("smtp.gmail.com", 587)
         server.starttls()
-        server.login(ALERT_EMAIL, "Singh#640")
+        server.login(ALERT_EMAIL, APP_PASS)
         message = f"Subject: ALERT!\n\n{service} is DOWN!"
-        server.sendmail(ALERT_EMAIL, ALERT_EMAIL, message)
+        server.sendmail(from_addr, to_addr, message)
         server.quit()
     except Exception as e:
         print("Alert failed:", e)
